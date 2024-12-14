9a37e2a4cdbeafa97081bf923e0ce9955520f4cc
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-13 19:03:35 -0800

    Update datetime.hpp making the date and time the parents

--- DateTime Utility Suite/datetime.hpp
+++ DateTime Utility Suite/datetime.hpp
@@ -1,6 +1,6 @@
 //
 //  datetime.hpp
-//  DateTime Utility Suite
+//  datetime2
 //
 //  Created by Isaac Adeleke on 12/12/24.
 //
@@ -10,48 +10,58 @@
 
 #include <string>
 
-// Base class for common functionality
-class DateTimeBase {
-public:
-    virtual std::string toString() const = 0; // Pure virtual function
-};
-
-// Time class inheriting from DateTimeBase
-class Time : public DateTimeBase {
-public:
-    int hour, minute, second;
-
-    Time(int hour = 0, int minute = 0, int second = 0);
-    void addSeconds(int seconds);
-    void addMinutes(int minutes);
-    void addHours(int hours);
-    std::string toString() const override;
-};
-
-// Date class inheriting from DateTimeBase
-class Date : public DateTimeBase {
-public:
+// Base class for Day
+class Day {
+protected:
     int year, month, day;
 
-    Date(int year = 1970, int month = 1, int day = 1);
+public:
+    Day(int year = 1970, int month = 1, int day = 1);
+    virtual ~Day() = default;
+
     bool isLeapYear(int year) const;
     int daysInMonth(int year, int month) const;
     void addDays(int days);
     void addMonths(int months);
     void addYears(int years);
-    std::string toString() const override;
+    std::string toString() const;
+    std::string dayOfWeek() const;
 };
 
-// DateTime class combining Date and Time
-class DateTime : public DateTimeBase {
-private:
-    Date date;
-    Time time;
+// Base class for Time
+class Time {
+protected:
+    int hour, minute, second;
 
 public:
-    DateTime(int year, int month, int day, int hour, int minute, int second);
+    Time(int hour = 0, int minute = 0, int second = 0);
+    virtual ~Time() = default;
+
+    void addSeconds(int seconds);
+    void addMinutes(int minutes);
+    void addHours(int hours);
+    std::string toString() const;
+};
+
+// Date class inheriting from Day
+class Date : public Day {
+public:
+    Date(int year = 1970, int month = 1, int day = 1);
+};
+
+// DateTime class inheriting from both Day and Time
+class DateTime : public Day, public Time {
+private:
+    int timeZoneOffset; // Time zone offset in minutes (e.g., -300 for EST)
+
+public:
+    DateTime(int year, int month, int day, int hour, int minute, int second, int timeZoneOffset = 0);
+
+    void adjustToTimeZone(int newTimeZoneOffset);
     static int differenceInSeconds(const DateTime& dt1, const DateTime& dt2);
-    std::string toString() const override;
+
+    std::string serialize() const;
+    static DateTime deserialize(const std::string& serialized);
     std::string format(const std::string& formatString) const;
 };
 
