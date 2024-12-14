ba3046b61cda9b266b89c208acce50a8cf188c94
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-13 16:31:41 -0800

    Update main.cpp- Refactored DateTime utility using inheritance for modularity and reusability

--- DateTime Utility Suite/main.cpp
+++ DateTime Utility Suite/main.cpp
@@ -5,10 +5,16 @@
 //  Created by Isaac Adeleke on 12/12/24.
 //
 
+#include "datetime.hpp"
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    DateTime dt(2024, 12, 13, 14, 30, 45);
+    std::cout << "Current DateTime: " << dt.toString() << std::endl;
+
+    dt.date.addDays(5);
+    dt.time.addHours(3);
+    std::cout << "Updated DateTime: " << dt.toString() << std::endl;
+
     return 0;
 }
