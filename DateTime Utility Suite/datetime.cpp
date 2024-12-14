2678df20b622bc3d26eedf3aade85f47cc5e6d1b
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-13 19:02:53 -0800

    Update datetime.cpp making the Date and time be the parents

--- DateTime Utility Suite/datetime.cpp
+++ DateTime Utility Suite/datetime.cpp
@@ -1,143 +1,68 @@
 //
-//  datetime.cpp
-//  DateTime Utility Suite
+//  datetime.hpp
+//  datetime2
 //
 //  Created by Isaac Adeleke on 12/12/24.
 //
 
-#include "datetime.hpp"
-#include <iomanip>
-#include <sstream>
-#include <cmath>
-using namespace std;
+#ifndef DATETIME_HPP
+#define DATETIME_HPP
 
-// Time Class Implementation
-Time::Time(int hour, int minute, int second) : hour(hour), minute(minute), second(second) {}
+#include <string>
 
-void Time::addSeconds(int seconds) {
-    second += seconds;
-    minute += second / 60;
-    second %= 60;
-    if (second < 0) {
-        second += 60;
-        minute--;
-    }
+// Base class for Day
+class Day {
+protected:
+    int year, month, day;
 
-    hour += minute / 60;
-    minute %= 60;
-    if (minute < 0) {
-        minute += 60;
-        hour--;
-    }
+public:
+    Day(int year = 1970, int month = 1, int day = 1);
+    virtual ~Day() = default;
 
-    hour %= 24;
-    if (hour < 0) hour += 24;
-}
+    bool isLeapYear(int year) const;
+    int daysInMonth(int year, int month) const;
+    void addDays(int days);
+    void addMonths(int months);
+    void addYears(int years);
+    std::string toString() const;
+    std::string dayOfWeek() const;
+};
 
-void Time::addMinutes(int minutes) {
-    addSeconds(minutes * 60);
-}
+// Base class for Time
+class Time {
+protected:
+    int hour, minute, second;
 
-void Time::addHours(int hours) {
-    addSeconds(hours * 3600);
-}
+public:
+    Time(int hour = 0, int minute = 0, int second = 0);
+    virtual ~Time() = default;
 
-std::string Time::toString() const {
-    ostringstream oss;
-    oss << setw(2) << setfill('0') << hour << ":"
-        << setw(2) << setfill('0') << minute << ":"
-        << setw(2) << setfill('0') << second;
-    return oss.str();
-}
+    void addSeconds(int seconds);
+    void addMinutes(int minutes);
+    void addHours(int hours);
+    std::string toString() const;
+};
 
-// Date Class Implementation
-Date::Date(int year, int month, int day) : year(year), month(month), day(day) {}
+// Date class inheriting from Day
+class Date : public Day {
+public:
+    Date(int year = 1970, int month = 1, int day = 1);
+};
 
-bool Date::isLeapYear(int year) const {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-}
+// DateTime class inheriting from both Day and Time
+class DateTime : public Day, public Time {
+private:
+    int timeZoneOffset; // Time zone offset in minutes (e.g., -300 for EST)
 
-int Date::daysInMonth(int year, int month) const {
-    if (month == 2) return isLeapYear(year) ? 29 : 28;
-    if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
-    return 31;
-}
+public:
+    DateTime(int year, int month, int day, int hour, int minute, int second, int timeZoneOffset = 0);
 
-void Date::addDays(int days) {
-    day += days;
-    while (day > daysInMonth(year, month)) {
-        day -= daysInMonth(year, month);
-        month++;
-        if (month > 12) {
-            month = 1;
-            year++;
-        }
-    }
-    while (day <= 0) {
-        month--;
-        if (month <= 0) {
-            month = 12;
-            year--;
-        }
-        day += daysInMonth(year, month);
-    }
-}
+    void adjustToTimeZone(int newTimeZoneOffset);
+    static int differenceInSeconds(const DateTime& dt1, const DateTime& dt2);
 
-void Date::addMonths(int months) {
-    month += months;
-    while (month > 12) {
-        month -= 12;
-        year++;
-    }
-    while (month <= 0) {
-        month += 12;
-        year--;
-    }
-    day = min(day, daysInMonth(year, month));
-}
+    std::string serialize() const;
+    static DateTime deserialize(const std::string& serialized);
+    std::string format(const std::string& formatString) const;
+};
 
-void Date::addYears(int years) {
-    year += years;
-    if (month == 2 && day == 29 && !isLeapYear(year)) {
-        day = 28;
-    }
-}
-
-std::string Date::toString() const {
-    ostringstream oss;
-    oss << setw(4) << setfill('0') << year << "-"
-        << setw(2) << setfill('0') << month << "-"
-        << setw(2) << setfill('0') << day;
-    return oss.str();
-}
-
-// DateTime Class Implementation
-DateTime::DateTime(int year, int month, int day, int hour, int minute, int second)
-    : date(year, month, day), time(hour, minute, second) {}
-
-int DateTime::differenceInSeconds(const DateTime& dt1, const DateTime& dt2) {
-    // Calculate days difference
-    int days1 = dt1.date.year * 365 + dt1.date.day;
-    for (int i = 1; i < dt1.date.month; ++i) {
-        days1 += dt1.date.daysInMonth(dt1.date.year, i);
-    }
-
-    int days2 = dt2.date.year * 365 + dt2.date.day;
-    for (int i = 1; i < dt2.date.month; ++i) {
-        days2 += dt2.date.daysInMonth(dt2.date.year, i);
-    }
-
-    int dayDifference = days1 - days2;
-    int timeDifference = (dt1.time.hour * 3600 + dt1.time.minute * 60 + dt1.time.second) -
-                         (dt2.time.hour * 3600 + dt2.time.minute * 60 + dt2.time.second);
-    return dayDifference * 86400 + timeDifference;
-}
-
-std::string DateTime::toString() const {
-    return date.toString() + " " + time.toString();
-}
-
-std::string DateTime::format(const std::string& formatString) const {
-    // Placeholder for custom formatting logic
-    return toString();
-}
+#endif
