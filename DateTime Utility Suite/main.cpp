76b21e924cf8cfe35120014412e7ecf41ed0c147
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-12-13 19:04:52 -0800

    Update main.cpp Making the Date and the Time the parents

--- DateTime Utility Suite/main.cpp
+++ DateTime Utility Suite/main.cpp
@@ -1,20 +1,29 @@
 //
 //  main.cpp
-//  DateTime Utility Suite
+//  datetime2
 //
 //  Created by Isaac Adeleke on 12/12/24.
 //
 
 #include "datetime.hpp"
 #include <iostream>
+using namespace std;
 
 int main() {
-    DateTime dt(2024, 12, 13, 14, 30, 45);
-    std::cout << "Current DateTime: " << dt.toString() << std::endl;
+    DateTime dt1(2023, 12, 10, 23, 59, 59, -300); // EST Time Zone
+    DateTime dt2(2023, 12, 9, 12, 30, 0, 0);      // UTC Time Zone
 
-    dt.date.addDays(5);
-    dt.time.addHours(3);
-    std::cout << "Updated DateTime: " << dt.toString() << std::endl;
+    cout << "DateTime 1: " << dt1.toString() << endl;
+    cout << "DateTime 2: " << dt2.toString() << endl;
+
+    dt1.adjustToTimeZone(0); // Convert to UTC
+    cout << "DateTime 1 (Converted to UTC): " << dt1.toString() << endl;
+
+    cout << "Day of the Week (dt2): " << dt2.dayOfWeek() << endl;
+
+    cout << "Serialized dt1: " << dt1.serialize() << endl;
+    DateTime dt3 = DateTime::deserialize("2023-12-10T23:59:59Z-300");
+    cout << "Deserialized dt3: " << dt3.toString() << endl;
 
     return 0;
 }
